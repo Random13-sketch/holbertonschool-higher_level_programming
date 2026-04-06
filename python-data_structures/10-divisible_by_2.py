e9721cab05ad9d96c2394267d166be153433a881
Random13-sketch <agazadenahid98@gmail.com>
2026-04-06 02:42:12 +0000

    uPDATE

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    for i in my_list:
-        if i % 2 == 0:
-            i = True
-        else:
-            i = False
-    return my_list
-
+    return [number % 2 == 0 for number in my_list]
